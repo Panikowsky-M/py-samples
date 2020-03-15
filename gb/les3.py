e36b84991f80ea2b5d282e7004052d3af09c0260
Panikowsky-M <panikowsky.m@gmail.com>
2020-03-15 13:07:04 +0300

    Дополнено решение задачи Угадай число

--- gb/les3.py
+++ gb/les3.py
@@ -3,14 +3,19 @@ import time
 yes = u'\u003D'
 no  = u'\u003C'
 no1 = u'\u003E'
+check = [yes,no,no1]
 print("Введите >,< или = для поиска числа.")
 print(random.randint(1,100))
+l1 = int(round(abs(random.randint(1,100) * 0.5 -100)))
 while True:
 	x_input = input()
 	if x_input == yes:
 		break
+	elif x_input != check:
+		print("Недопустимый управляющий символ!")
+		break
 	elif x_input == no:
-		print(random.randint(1,100))
+		print(random.randint(1,l1))
 		time.sleep(1)
 	elif x_input == no1:
 		print(random.randint(1,100))
