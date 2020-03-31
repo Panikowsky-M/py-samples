179097acdd0f65379c847c304e948b7d15835321
Panikowsky-M <panikowsky.m@gmail.com>
2020-03-31 20:57:34 +0300

    Revert "Попытка решить задачу с обработкой текста 310320"
    This reverts commit e178419af4c56ff2a5427b9cc9b75007ed89219f.

--- a_489.py
+++
@@ -1,7 +0,0 @@
-ls = [2000,10003,1234000,44444444,9999,11,11,22,123]
-def weightSort(integers):
-	res = sorted([str(x) for x in integers])
-	res = sorted(res,key=lambda x: sum(int(d) for d in x))
-	return res
-
-print(weightSort(ls))
--- passwd-gen.py
+++ passwd-gen.py
@@ -5,8 +5,7 @@ import random
 s1_signs = '!@#$%^&*()+=_-'
 s0 = string.digits + string.ascii_letters + s1_signs
 passwd = []
-n = int(input())
-for i in range(0,n):
+for i in range(0,15):
 	passwd+= random.choice(s0)
 	time.sleep(0)
 print(str(passwd))
