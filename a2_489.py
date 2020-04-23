eb37a97e38a24478cb3b3ee9e4b6ce17d2f37f09
Panikowsky-M <panikowsky.m@gmail.com>
2020-04-23 14:51:42 +0300

    Решаю задачу task2_489

--- a2_489.py
+++ a2_489.py
@@ -5,10 +5,17 @@ text = str(f.readlines())
 print("%s\n" % text)
 
 words = re.findall('[a-zA-Z]+',text)
-print("%s\n" % words)
+#print("%s\n" % words)
 
 stats = {}
 for w in words:
     stats[w] = stats.get(w,0) + 1
 
-print(stats)
+w_ranks = sorted(stats.items(), key = lambda x : x[1],\
+      reverse=True) [:10]
+_wrex = re.findall('[a-zA-Z]+',str(w_ranks))
+_drex = re.findall('[0-9]+',str(w_ranks))
+pl = [p for p in range(1,10)]
+for j in range(len(_wrex)-1):
+    places = '{} place,{} - {} times'.format(pl[j],_wrex[j],_drex[j])
+    print(places) 
