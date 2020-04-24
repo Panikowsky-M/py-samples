287d4a8e91808b7127e12197c20d9139f370fa3c
Panikowsky-M <panikowsky.m@gmail.com>
2020-04-24 13:21:24 +0300

    Доработка task2_489

--- a2_489.py
+++ a2_489.py
@@ -2,20 +2,22 @@ import re
 
 f = open('wiki_text.txt','r')
 text = str(f.readlines())
-print("%s\n" % text)
+#print("%s\n" % text)
 
-words = re.findall('[a-zA-Z]+',text)
+words = re.findall('[a-zA-Z]{2,}',text) #Регулярка для слов от двух букв
 #print("%s\n" % words)
 
 stats = {}
 for w in words:
     stats[w] = stats.get(w,0) + 1
 
+print(stats)
 w_ranks = sorted(stats.items(), key = lambda x : x[1],\
-      reverse=True) [:10]
+      reverse=True) [0:10]
+print(w_ranks)
 _wrex = re.findall('[a-zA-Z]+',str(w_ranks))
 _drex = re.findall('[0-9]+',str(w_ranks))
-pl = [p for p in range(1,10)]
-for j in range(len(_wrex)-1):
+pl = [p for p in range(1,11)]
+for j in range(len(_wrex)):
     places = '{} place,{} - {} times'.format(pl[j],_wrex[j],_drex[j])
-    print(places) 
+    print(places)
