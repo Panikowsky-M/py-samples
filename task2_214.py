c7985f3a1b9cd976cdedd115582aa955c8a58dfc
Panikowsky-M <panikowsky.m@gmail.com>
2020-05-18 14:19:24 +0300

    Решена задача на проверку пароля 2_214

--- task2_214.py
+++ task2_214.py
@@ -9,35 +9,37 @@
 
 import traceback 
 import re
+# import string
 
 
 def check_password(password):
-	ok = "valid"
-	error = "not valid"
-	v = -5
-	l = len(password)
-	if l >= 8:
-		v+=1
-		if l <= 20:
-			v+=1
-			if(re.search('[a-zA-z]',password)):
-				v+=1
-				if (re.search('[0-9]',password)):
-					v+=1
-					if (re.search('[!@#$%^&*_,.:;]',password)):
-						v+=1
-	if v == 0:
-		return ok
-	else: return error
+    mOb = re.match('[a-zA-Z0-9^\.]+',password)
+    seOblowL = re.search('[a-z]+',password)
+    seObupL = re.search('[A-Z]+',password)
+    seObD = re.search('[0-9]+',password)
+    if len(password) >= 8 and len(password) <= 20:
+        #print(len(password))
+        if mOb:
+            if seOblowL and seObupL and seObD:
+                return 'valid'
+            else:
+                return 'not valid'
+        else:
+             return 'not valid'
+    else:
+        return 'not valid'
+
+
+#print(check_password("P1@pP1@pP1@pP1@pP1@pP1@p"))
 
 # Тесты
 try:
-    assert check_password("") == "not valid"
-    assert check_password("password") == "not valid"
-    assert check_password("P1@p") == "not valid"
-    assert check_password("P1@pP1@p") == "valid"
-    assert check_password("P1@pP1@pP1@pP1@pP1@pP1@p") == "not valid"
-    assert check_password("Paaaaaa222!!!") == "valid"
+   assert check_password("") == "not valid"
+   assert check_password("password") == "not valid"
+   assert check_password("P1@p") == "not valid"
+   assert check_password("P1@pP1@p") == "valid"
+   assert check_password("P1@pP1@pP1@pP1@pP1@pP1@p") == "not valid"
+   assert check_password("Paaaaaa222!!!") == "valid"
 except AssertionError:
     print("TEST ERROR")
     traceback.print_exc()
