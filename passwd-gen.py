f0403beb905d3808f08e9ebfe59fef89ee94dbea
Panikowsky-M <panikowsky.m@gmail.com>
2020-09-28 22:20:39 +0000

    Кодирует в base64 и выводит длину

--- passwd-gen.py
+++ passwd-gen.py
@@ -1,6 +1,7 @@
 import time
 import string
 import random
+import base64
 
 s1_signs = '!@#$%^&*()+=_-'
 s0 = string.digits + string.ascii_letters + s1_signs
@@ -10,5 +11,13 @@ for i in range(0,15):
 	time.sleep(0)
 result = ''
 for i in passwd:
-    result+=i
-print(result)
+    result+= i 
+passLen = len(result)
+print("Сырой пароль: "+result + "\nдлина пароля: %d" % passLen)
+
+base = result.encode("UTF-8")
+enc = base64.b64encode(base)
+b64pass = enc.decode("UTF-8")
+print("\n--- \n"+"Закодированый в base64: "+b64pass)
+passLen = len(b64pass)
+print("Длина пароля: %d" % passLen)
