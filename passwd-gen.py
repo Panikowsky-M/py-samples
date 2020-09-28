77c0ac656e8ad19f86b0ec63b6f98fe6d864cde3
Panikowsky-M <panikowsky.m@gmail.com>
2020-09-28 22:48:41 +0000

    Кодирую пароль в base64

--- passwd-gen.py
+++ passwd-gen.py
@@ -3,21 +3,34 @@ import string
 import random
 import base64
 
+lowRange = 0
+highRange = int(input("Введите длину пароля: "))
 s1_signs = '!@#$%^&*()+=_-'
 s0 = string.digits + string.ascii_letters + s1_signs
 passwd = []
-for i in range(0,15):
+for i in range(0,30):
 	passwd+= random.choice(s0)
 	time.sleep(0)
-result = ''
+passwd=passwd[lowRange:highRange]
+
+result=''
 for i in passwd:
-    result+= i 
+    result+= i
+
 passLen = len(result)
 print("Сырой пароль: "+result + "\nдлина пароля: %d" % passLen)
+passwd = []
 
 base = result.encode("UTF-8")
 enc = base64.b64encode(base)
 b64pass = enc.decode("UTF-8")
-print("\n--- \n"+"Закодированый в base64: "+b64pass)
-passLen = len(b64pass)
+passwd+=b64pass
+passwd=passwd[lowRange:highRange]
+
+result=''
+for i in passwd:
+    result+= i
+
+print("\n--- \n"+"Закодированый в base64: "+result)
+passLen = len(result)
 print("Длина пароля: %d" % passLen)
