388c3d0a9628aa8ee0af6c80ac85931f386978bc
Panikowsky-M <panikowsky.m@gmail.com>
2020-10-19 17:15:13 +0000

    Поиск по репозиторию по названию пакета

--- catpypacket/catpacinf.py
+++ catpypacket/catpacinf.py
@@ -1,22 +1,30 @@
-import requests, tarfile
+import requests
+import xml.etree.ElementTree as xmlElement
+import tarfile
 
+
+print("Вводите название пакета:\n")
+SEARCH = input()
 FILENAME = 'testfile.tar.gz'
-PATH = 'packet_extract'
-META = 'generate_wiki-1.2.0/generate_wiki.egg-info/PKG-INFO'
+LINK  = str('https://pypi.org/simple/%s' % SEARCH)
 
-#print('Загрузка сценария женкинса ... ')
-print('Загрузка пакета ... ')
+def printRepo(name):
+    rq = requests.get(LINK)
+    print('Статус запроса - %s'%(rq.status_code))
+    print('MIME загружаемого файла - %s'%(rq.headers['content-type']))
+    print('Кодировка - %s'%(rq.encoding))
+    docRoot = xmlElement.fromstring(rq.content)
+    pack_link = None
+    for el in docRoot[1]:
+        if el.tag == "a":
+            url = el.attrib["href"]
+            if ".whl#" in url:
+                pack_link = url
+    return pack_link
 
-#fileToLoad = 'https://raw.githubusercontent.com/Panikowsky-M/docker/master/pipes/build_docker_pipe.jenkins'
-fileToLoad  = 'https://files.pythonhosted.org/packages/57/54/5bff2bd5df319dd172610d9bf196040e28d7dcb1b6f074a7d9b908a37f9c/generate_wiki-1.2.0.tar.gz'
+print(printRepo(SEARCH))
 
-rq = requests.get(fileToLoad)
-
-print('Статус запроса - %s'%(rq.status_code))
-print('MIME загруженного файла - %s'%(rq.headers['content-type']))
-print('Кодировка - %s'%(rq.encoding))
-
-
+"""
 print('Пакет загружен')
 with open(FILENAME,'wb') as _file:
     _file.write(rq.content)
@@ -35,7 +43,6 @@ with tarfile.open(FILENAME,"r") as arc:
     print("\nМетаданные пакета:\n====\n" +\
           meta.read().decode('utf-8') )
 
-    """
     for inf in arc.getmembers():
         if fnmatch.fnmatch(inf.name,META):
             print(inf.name)
